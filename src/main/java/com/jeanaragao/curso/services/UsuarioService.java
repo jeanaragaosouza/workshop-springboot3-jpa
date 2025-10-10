88dc81c702f5651cac9690faa6da024836c89114
jeanaragaosouza <tuning.jean@hotmail.com>
2025-10-09 23:34:19 -0300

    Service layer, component registration

---
+++ src/main/java/com/jeanaragao/curso/services/UsuarioService.java
@@ -0,0 +1,28 @@
+package com.jeanaragao.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jeanaragao.curso.entities.Usuario;
+import com.jeanaragao.curso.repositories.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+
+	@Autowired
+	private UsuarioRepository repository;
+	
+	public List<Usuario> findAll(){
+		return repository.findAll();
+	}
+	
+	
+	public Usuario findById(Long id) {
+	Optional<Usuario> obj =	repository.findById(id);
+	return obj.get();
+	}
+	
+}
