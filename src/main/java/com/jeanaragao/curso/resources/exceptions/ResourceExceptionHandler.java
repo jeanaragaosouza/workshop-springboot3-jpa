0a426cafe9cc9451314f69527c97b37e853d90d6
jeanaragaosouza <tuning.jean@hotmail.com>
2025-10-16 21:32:20 -0300

    Exception handling - delete

--- src/main/java/com/jeanaragao/curso/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/jeanaragao/curso/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.jeanaragao.curso.services.exceptions.DataBaseException;
 import com.jeanaragao.curso.services.exceptions.ResourceNotFoundExceptions;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -22,5 +23,16 @@ public class ResourceExceptionHandler {
 				request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
+	
+	@ExceptionHandler(DataBaseException.class)
+	public ResponseEntity<StandardError> dataBase(DataBaseException e, HttpServletRequest request) {
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+				request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	
 
 }
--- src/main/java/com/jeanaragao/curso/services/UsuarioService.java
+++ src/main/java/com/jeanaragao/curso/services/UsuarioService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.jeanaragao.curso.entities.Usuario;
 import com.jeanaragao.curso.repositories.UsuarioRepository;
+import com.jeanaragao.curso.services.exceptions.DataBaseException;
 import com.jeanaragao.curso.services.exceptions.ResourceNotFoundExceptions;
 
 @Service
@@ -26,13 +29,22 @@ public class UsuarioService {
 	}
 
 	public Usuario insert(Usuario obj) {
+
 		return repository.save(obj);
+
 	}
-	
+
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundExceptions(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
+		
 	}
-	
+
 	public Usuario update(Long id, Usuario obj) {
 		Usuario entity = repository.getReferenceById(id);
 		updateData(entity, obj);
@@ -43,7 +55,7 @@ public class UsuarioService {
 		entity.setNome(obj.getNome());
 		entity.setEmail(obj.getEmail());
 		entity.setTelefone(obj.getTelefone());
-		
+
 	}
-	
+
 }
